bcecb66e8d9fccfe091a7c6cf84c2f27f9b04858
nikhil-sde <nikhil3h@gmail.com>
2025-10-02 08:22:14 -0400

    version1

---
+++ src/main/java/com/mycompany/inclassexercise2/InClassExercise2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inclassexercise2;
+
+/**
+ *
+ * @author nikhil
+ */
+public class InClassExercise2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
