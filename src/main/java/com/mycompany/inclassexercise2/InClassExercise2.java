e5cbe3038607c3eed66579a8c9703ad53c95df6f
nikhil-sde <nikhil3h@gmail.com>
2025-10-02 08:49:47 -0400

    version3

--- src/main/java/com/mycompany/inclassexercise2/InClassExercise2.java
+++ src/main/java/com/mycompany/inclassexercise2/InClassExercise2.java
@@ -12,6 +12,6 @@ public class InClassExercise2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("version2");
+        System.out.println("version3");
     }
 }
